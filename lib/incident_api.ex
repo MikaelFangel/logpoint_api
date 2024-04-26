f96f8185146748681c1f5030227e3e3dbe27bcfc
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-04-26 14:08:59 +0200

    incident_api: added specs

--- lib/incident_api.ex
+++ lib/incident_api.ex
@@ -11,15 +11,19 @@ defmodule LogpointApi.IncidentApi do
     defstruct [:incident_obj_id, :incident_id]
   end
 
+  @spec get_incidents(String.t(), Credential.t(), TimeRange.t()) :: {:ok, map()} | {:error, String.t()}
   def get_incidents(ip, credential, %TimeRange{} = time_range),
     do: get_incident_information(ip, "/incidents", credential, time_range)
 
+  @spec get_data_from_incident(String.t(), Credential.t(), Incident.t()) :: {:ok, map()} | {:error, String.t()}
   def get_data_from_incident(ip, credential, %Incident{} = incident),
     do: get_incident_information(ip, "/get_data_from_incident", credential, incident)
 
+  @spec get_incident_states(String.t(), Credential.t(), TimeRange.t()) :: {:ok, map()} | {:error, String.t()}
   def get_incident_states(ip, credential, %TimeRange{} = time_range),
     do: get_incident_information(ip, "/incident_states", credential, time_range)
 
+  @spec get_users(String.t(), Credential.t()) :: {:ok, map()} | {:error, String.t()}
   def get_users(ip, %Credential{} = credential) do
     params = %{
       "username" => credential.username,
@@ -29,6 +33,7 @@ defmodule LogpointApi.IncidentApi do
     make_request(ip, "/get_users", params)
   end
 
+  @spec get_incident_information(String.t(), String.t(), Credential.t(), map()) :: {:ok, map()} | {:error, String.t()}
   defp get_incident_information(ip, path, %Credential{} = credential, request_data) do
     params = %{
       "username" => credential.username,
@@ -39,6 +44,7 @@ defmodule LogpointApi.IncidentApi do
     make_request(ip, path, params)
   end
 
+  @spec make_request(String.t(), String.t(), map()) :: {:ok, map()} | {:error, String.t()}
   defp make_request(ip, path, params) do
     url = build_url(ip, path)
     headers = [{"Content-Type", "application/json"}]
@@ -58,5 +64,6 @@ defmodule LogpointApi.IncidentApi do
     end
   end
 
+  @spec build_url(String.t(), String.t()) :: String.t()
   defp build_url(ip, path), do: "https://" <> ip <> path
 end
