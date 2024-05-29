1758fc58601ac4a68b19310f92ca70490411ea74
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-05-29 10:04:55 +0200

    logpoint_api: add initial run_search function

--- lib/logpoint_api.ex
+++ lib/logpoint_api.ex
@@ -1,11 +1,46 @@
 defmodule LogpointApi do
   @moduledoc false
 
+  alias LogpointApi.SearchApi.Query
+  alias LogpointApi.SearchApi
+
   defmodule Credential do
     @typedoc """
     Struct representing credentials used for authorization.
     """
     @type t :: %__MODULE__{username: String.t(), secret_key: String.t()}
     defstruct [:username, :secret_key]
+  end
+
+  @spec run_search(String.t(), Credential.t(), Query.t()) :: {:ok, map()} | {:error, String.t()}
+  def run_search(ip, credential, %Query{} = query) do
+    {:ok, search_info} = SearchApi.get_search_id(ip, credential, query)
+    search_id = Map.get(search_info, "search_id")
+
+    SearchApi.get_search_result(ip, credential, search_id)
+    |> handle_search_result(ip, credential, search_id, query)
+  end
+
+  defp handle_search_result({:ok, %{"final" => true} = result}, _, _, _, _), do: result
+
+  defp handle_search_result(
+         {:ok, %{"final" => false, "succes" => true}},
+         ip,
+         credential,
+         search_id,
+         query
+       ) do
+    SearchApi.get_search_result(ip, credential, search_id)
+    |> handle_search_result(ip, credential, search_id, query)
+  end
+
+  defp handle_search_result(
+         {:ok, %{"final" => false, "success" => false}},
+         ip,
+         credential,
+         _,
+         query
+       ) do
+    run_search(ip, credential, query)
   end
 end
