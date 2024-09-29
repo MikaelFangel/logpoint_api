0ee247c955f16378002f2292968ea1e2447c6b3f
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-09-29 11:27:03 +0200

    mix: add credo

--- mix.exs
+++ mix.exs
@@ -26,7 +26,8 @@ defmodule LogpointApi.MixProject do
     [
       {:httpoison, "~> 2.0"},
       {:jason, "~> 1.4"},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: :dev, runtime: false}
     ]
   end
 end
