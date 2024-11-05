8f14db9922373112bf1c3e5f4360643941f57d8d
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-11-05 15:10:59 +0100

    feat!: introduce the client struct
    Removes the credential struct in favour of the client struct, because
    the ip is used every time the credentials is used.
    
    Remove functions that only is changing a string before making the call.

--- lib/incident_api.ex
+++ lib/incident_api.ex
@@ -3,7 +3,7 @@ defmodule LogpointApi.IncidentApi do
   This module provides an implementation of the Logpoint Incident API.
   """
 
-  alias LogpointApi.Credential
+  alias LogpointApi.Client
 
   defmodule TimeRange do
     @typedoc """
@@ -53,92 +53,87 @@ defmodule LogpointApi.IncidentApi do
   @doc """
   Get all incidents within a given time range.
   """
-  @spec get_incidents(String.t(), Credential.t(), TimeRange.t()) ::
-          {:ok, map()} | {:error, String.t()}
-  def get_incidents(ip, credential, %TimeRange{} = time_range),
-    do: get_incident_information(ip, "/incidents", credential, time_range)
+  @spec get_incidents(Client.t(), TimeRange.t()) :: {:ok, map()} | {:error, String.t()}
+  def get_incidents(client, %TimeRange{} = time_range),
+    do: get_incident_information(client, "/incidents", time_range)
 
   @doc """
   Get a specific incident and its related data.
   """
-  @spec get_data_from_incident(String.t(), Credential.t(), Incident.t()) ::
-          {:ok, map()} | {:error, String.t()}
-  def get_data_from_incident(ip, credential, %Incident{} = incident),
-    do: get_incident_information(ip, "/get_data_from_incident", credential, incident)
+  @spec get_data_from_incident(Client.t(), Incident.t()) :: {:ok, map()} | {:error, String.t()}
+  def get_data_from_incident(client, %Incident{} = incident),
+    do: get_incident_information(client, "/get_data_from_incident", incident)
 
   @doc """
   Get the states of incidents within a specific time range.
   """
-  @spec get_incident_states(String.t(), Credential.t(), TimeRange.t()) ::
+  @spec get_incident_states(Client.t(), TimeRange.t()) ::
           {:ok, map()} | {:error, String.t()}
-  def get_incident_states(ip, credential, %TimeRange{} = time_range),
-    do: get_incident_information(ip, "/incident_states", credential, time_range)
+  def get_incident_states(client, %TimeRange{} = time_range),
+    do: get_incident_information(client, "/incident_states", time_range)
 
   @doc """
   Get users.
   """
-  @spec get_users(String.t(), Credential.t()) :: {:ok, map()} | {:error, String.t()}
-  def get_users(ip, credential) do
-    payload = make_payload(credential)
-    make_request(ip, "/get_users", :get, payload)
+  @spec get_users(Client.t()) :: {:ok, map()} | {:error, String.t()}
+  def get_users(client) do
+    payload = make_payload(client)
+    make_request(client.ip, "/get_users", :get, payload)
   end
 
   @doc """
   Add comments to a list of incidents.
   """
-  @spec add_comments(String.t(), Credential.t(), IncidentCommentData.t()) ::
-          {:ok, map()} | {:error, String.t()}
-  def add_comments(ip, credential, %IncidentCommentData{} = incident_comment_data),
-    do: update_incident_state(ip, "/add_incident_comment", credential, incident_comment_data)
+  @spec add_comments(Client.t(), IncidentCommentData.t()) :: {:ok, map()} | {:error, String.t()}
+  def add_comments(client, %IncidentCommentData{} = incident_comment_data),
+    do: update_incident_state(client, "/add_incident_comment", incident_comment_data)
 
   @doc """
   Assign or re-assign a list of incidents.
   """
-  @spec assign_incidents(String.t(), Credential.t(), IncidentIDs.t(), String.t()) ::
+  @spec assign_incidents(Client.t(), IncidentIDs.t(), String.t()) ::
           {:ok, map()} | {:error, String.t()}
-  def assign_incidents(ip, credential, %IncidentIDs{} = incident_ids, assignee_id) do
+  def assign_incidents(client, %IncidentIDs{} = incident_ids, assignee_id) do
     payload = Map.put(incident_ids, :new_assignee, assignee_id)
-    update_incident_state(ip, "/assign_incident", credential, payload)
+    update_incident_state(client, "/assign_incident", payload)
   end
 
   @doc """
   Resolve a list of incidents.
   """
-  @spec resolve_incidents(String.t(), Credential.t(), IncidentIDs.t()) ::
-          {:ok, map()} | {:error, String.t()}
-  def resolve_incidents(ip, credential, %IncidentIDs{} = incident_ids),
-    do: update_incident_state(ip, "/resolve_incident", credential, incident_ids)
+  @spec resolve_incidents(Client.t(), IncidentIDs.t()) :: {:ok, map()} | {:error, String.t()}
+  def resolve_incidents(client, %IncidentIDs{} = incident_ids),
+    do: update_incident_state(client, "/resolve_incident", incident_ids)
 
   @doc """
   Reopen a list of incidents.
   """
-  @spec reopen_incidents(String.t(), Credential.t(), IncidentIDs.t()) ::
-          {:ok, map()} | {:error, String.t()}
-  def reopen_incidents(ip, credential, %IncidentIDs{} = incident_ids),
-    do: update_incident_state(ip, "/reopen_incident", credential, incident_ids)
+  @spec reopen_incidents(Client.t(), IncidentIDs.t()) :: {:ok, map()} | {:error, String.t()}
+  def reopen_incidents(client, %IncidentIDs{} = incident_ids),
+    do: update_incident_state(client, "/reopen_incident", incident_ids)
 
   @doc """
   Close a list of incidents.
   """
-  @spec close_incidents(String.t(), Credential.t(), IncidentIDs.t()) ::
+  @spec close_incidents(Client.t(), IncidentIDs.t()) ::
           {:ok, map()} | {:error, String.t()}
-  def close_incidents(ip, credential, %IncidentIDs{} = incident_ids),
-    do: update_incident_state(ip, "/close_incident", credential, incident_ids)
+  def close_incidents(client, %IncidentIDs{} = incident_ids),
+    do: update_incident_state(client, "/close_incident", incident_ids)
 
   @doc false
-  @spec update_incident_state(String.t(), String.t(), Credential.t(), map()) ::
+  @spec update_incident_state(Client.t(), String.t(), map()) ::
           {:ok, map()} | {:error, String.t()}
-  defp update_incident_state(ip, path, credential, request_data) do
-    payload = make_payload(credential, request_data)
-    make_request(ip, path, :post, payload)
+  defp update_incident_state(client, path, request_data) do
+    payload = make_payload(client, request_data)
+    make_request(client.ip, path, :post, payload)
   end
 
   @doc false
-  @spec get_incident_information(String.t(), String.t(), Credential.t(), map()) ::
+  @spec get_incident_information(Client.t(), String.t(), map()) ::
           {:ok, map()} | {:error, String.t()}
-  defp get_incident_information(ip, path, credential, request_data) do
-    payload = make_payload(credential, request_data)
-    make_request(ip, path, :get, payload)
+  defp get_incident_information(client, path, request_data) do
+    payload = make_payload(client, request_data)
+    make_request(client.ip, path, :get, payload)
   end
 
   @doc false
@@ -167,22 +162,22 @@ defmodule LogpointApi.IncidentApi do
   defp build_url(ip, path), do: "https://" <> ip <> path
 
   @doc false
-  @spec make_payload(Credential.t(), map()) :: String.t()
-  defp make_payload(%Credential{} = credential, request_data) do
+  @spec make_payload(Client.t(), map()) :: String.t()
+  defp make_payload(%Client{} = client, request_data) do
     %{
-      "username" => credential.username,
-      "secret_key" => credential.secret_key,
+      "username" => client.username,
+      "secret_key" => client.secret_key,
       "requestData" => request_data
     }
     |> Jason.encode!()
   end
 
   @doc false
-  @spec make_payload(Credential.t()) :: String.t()
-  defp make_payload(%Credential{} = credential) do
+  @spec make_payload(Client.t()) :: String.t()
+  defp make_payload(%Client{} = client) do
     %{
-      "username" => credential.username,
-      "secret_key" => credential.secret_key
+      "username" => client.username,
+      "secret_key" => client.secret_key
     }
     |> Jason.encode!()
   end
--- lib/search_api.ex
+++ lib/search_api.ex
@@ -3,9 +3,9 @@ defmodule LogpointApi.SearchApi do
   This module provides an implementation of the Logpoint search API.
   """
 
-  alias LogpointApi.Credential
+  alias LogpointApi.Client
 
-  @allowed_types ["user_preference", "loginspects", "logpoint_repos", "devices", "livesearches"]
+  @allowed_types [:user_preference, :loginspects, :logpoint_repos, :devices, :livesearches]
 
   defmodule Query do
     @typedoc """
@@ -21,65 +21,19 @@ defmodule LogpointApi.SearchApi do
     defstruct [:query, :time_range, :limit, :repos]
   end
 
-  defmodule SearchID do
-    @typedoc """
-    Struct representing a search id.
-    """
-    @type t :: %__MODULE__{search_id: String.t()}
-    @derive {Jason.Encoder, only: [:search_id]}
-    defstruct [:search_id]
-  end
-
-  @doc """
-  Get the current users time zone information.
-  """
-  @spec get_user_timezone(String.t(), Credential.t()) :: {:ok, map()} | {:error, String.t()}
-  def get_user_timezone(ip, credential),
-    do: get_allowed_data(ip, credential, "user_preference")
-
-  @doc """
-  Get all logpoints of a Logpoint instance.
-  """
-  @spec get_logpoints(String.t(), Credential.t()) :: {:ok, map()} | {:error, String.t()}
-  def get_logpoints(ip, credential),
-    do: get_allowed_data(ip, credential, "loginspects")
-
-  @doc """
-  Get the repos of an instance.
-  """
-  @spec get_repos(String.t(), Credential.t()) :: {:ok, map()} | {:error, String.t()}
-  def get_repos(ip, credential),
-    do: get_allowed_data(ip, credential, "logpoint_repos")
-
-  @doc """
-  Get the devices of the instance.
-  """
-  @spec get_devices(String.t(), Credential.t()) :: {:ok, map()} | {:error, String.t()}
-  def get_devices(ip, credential),
-    do: get_allowed_data(ip, credential, "devices")
-
-  @doc """
-  Get all live search on a machine.
-  """
-  @spec get_livesearches(String.t(), Credential.t()) :: {:ok, map()} | {:error, String.t()}
-  def get_livesearches(ip, credential),
-    do: get_allowed_data(ip, credential, "livesearches")
-
   @doc """
   Create a search and get its search id.
   """
-  @spec get_search_id(String.t(), Credential.t(), Query.t()) ::
-          {:ok, map()} | {:error, String.t()}
-  def get_search_id(ip, credential, %Query{} = query),
-    do: get_search_logs(ip, credential, query)
+  @spec get_search_id(Client.t(), Query.t()) :: {:ok, map()} | {:error, String.t()}
+  def get_search_id(client, %Query{} = query),
+    do: get_search_logs(client, query)
 
   @doc """
   Retrieve the search result of a specific search id.
   """
-  @spec get_search_result(String.t(), Credential.t(), String.t()) ::
-          {:ok, map()} | {:error, String.t()}
-  def get_search_result(ip, credential, search_id),
-    do: get_search_logs(ip, credential, %SearchID{search_id: search_id})
+  @spec get_search_result(Client.t(), String.t()) :: {:ok, map()} | {:error, String.t()}
+  def get_search_result(client, search_id),
+    do: get_search_logs(client, %{search_id: search_id})
 
   @doc false
   @spec make_request(String.t(), String.t(), String.t()) :: {:ok, map()} | {:error, String.t()}
@@ -106,27 +60,26 @@ defmodule LogpointApi.SearchApi do
   defp build_url(ip, path), do: "https://" <> ip <> path
 
   @doc false
-  @spec get_allowed_data(String.t(), Credential.t(), String.t()) ::
-          {:ok, map()} | {:error, String.t()}
-  defp get_allowed_data(ip, credential, type) when type in @allowed_types do
-    payload = build_payload(credential, %{"type" => type})
-    make_request(ip, "/getalloweddata", payload)
+  @spec get_allowed_data(Client.t(), String.t()) :: {:ok, map()} | {:error, String.t()}
+  def get_allowed_data(client, type) when type in @allowed_types do
+    payload = build_payload(client, %{"type" => Atom.to_string(type)})
+    make_request(client.ip, "/getalloweddata", payload)
   end
 
   @doc false
-  @spec get_search_logs(String.t(), Credential.t(), map()) :: {:ok, map()} | {:error, String.t()}
-  defp get_search_logs(ip, credential, request_data) do
-    payload = build_payload(credential, %{"requestData" => Jason.encode!(request_data)})
-    make_request(ip, "/getsearchlogs", payload)
+  @spec get_search_logs(Client.t(), map()) :: {:ok, map()} | {:error, String.t()}
+  defp get_search_logs(client, request_data) do
+    payload = build_payload(client, %{"requestData" => Jason.encode!(request_data)})
+    make_request(client.ip, "/getsearchlogs", payload)
   end
 
   @doc false
-  @spec build_payload(Credential.t(), map()) :: String.t()
-  defp build_payload(%Credential{} = credential, data) do
+  @spec build_payload(Client.t(), map()) :: String.t()
+  defp build_payload(%Client{} = client, data) do
     Map.merge(
       %{
-        "username" => credential.username,
-        "secret_key" => credential.secret_key
+        "username" => client.username,
+        "secret_key" => client.secret_key
       },
       data
     )
