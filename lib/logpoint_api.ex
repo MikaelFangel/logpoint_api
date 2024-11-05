2a9f647ed6b553be82f4b803f71c6b10368ea7da
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-11-05 15:10:59 +0100

    feat!: introduce the client struct
    Removes the credential struct in favour of the client struct, because
    the ip is used every time the credentials is used.
    
    Remove functions that only is changing a string before making the call.

--- lib/logpoint_api.ex
+++ lib/logpoint_api.ex
@@ -6,53 +6,56 @@ defmodule LogpointApi do
   alias LogpointApi.SearchApi.Query
   alias LogpointApi.SearchApi
 
-  defmodule Credential do
+  defmodule Client do
     @typedoc """
     Struct representing credentials used for authorization.
     """
-    @type t :: %__MODULE__{username: String.t(), secret_key: String.t()}
-    defstruct [:username, :secret_key]
+    @type t :: %__MODULE__{ip: String.t(), username: String.t(), secret_key: String.t()}
+    defstruct [:ip, :username, :secret_key]
+
+    @spec new(String.t(), String.t(), String.t()) :: t()
+    def new(ip, username, secret_key) do
+      %Client{ip: ip, username: username, secret_key: secret_key}
+    end
   end
 
   @doc """
   Run a search query.
   """
-  @spec run_search(String.t(), Credential.t(), Query.t()) :: map()
-  def run_search(ip, credential, %Query{} = query) do
-    {:ok, %{"success" => true} = search_info} = SearchApi.get_search_id(ip, credential, query)
+  @spec run_search(Client.t(), Query.t()) :: map()
+  def run_search(client, %Query{} = query) do
+    {:ok, %{"success" => true} = search_info} = SearchApi.get_search_id(client, query)
     search_id = Map.get(search_info, "search_id")
 
-    SearchApi.get_search_result(ip, credential, search_id)
-    |> handle_search_result(ip, credential, search_id, query)
+    SearchApi.get_search_result(client, search_id)
+    |> handle_search_result(client, search_id, query)
   end
 
-  defp handle_search_result({:ok, %{"final" => true} = result}, _, _, _, _), do: result
+  defp handle_search_result({:ok, %{"final" => true} = result}, _, _, _), do: result
 
   defp handle_search_result(
          {:ok, %{"final" => false}},
-         ip,
-         credential,
+         client,
          search_id,
          query
        ) do
-    result = SearchApi.get_search_result(ip, credential, search_id)
+    result = SearchApi.get_search_result(client, search_id)
 
     # Wait before retrying.
     :timer.sleep(1000)
 
-    handle_search_result(result, ip, credential, search_id, query)
+    handle_search_result(result, client, search_id, query)
   end
 
   defp handle_search_result(
          {:ok, %{"success" => false}},
-         ip,
-         credential,
+         client,
          _,
          query
        ) do
     # Wait before recreating the search.
     :timer.sleep(1000)
 
-    run_search(ip, credential, query)
+    run_search(client, query)
   end
 end
