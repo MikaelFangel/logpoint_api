899f5b3c8b9497e85e00e4ec4e58ce1274b74951
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-12-03 19:36:15 +0100

    chore: simplify and add the other client functions

--- lib/logpoint.ex
+++ lib/logpoint.ex
@@ -6,27 +6,12 @@ defmodule Logpoint do
   alias Logpoint.Client.Supervisor
   alias Logpoint.Client
 
-  def start_supervisor do
-    Supervisor.start_link(nil)
-  end
-
-  def new(ip, username, secret_key) do
-    Supervisor.start_client(ip, username, secret_key)
-  end
-
-  def submit_search(pid, query) do
-    Client.submit_search(pid, query)
-  end
-
-  def search_status(pid, search_id) do
-    Client.search_status(pid, search_id)
-  end
-
-  def search_result(pid, search_id) do
-    Client.search_result(pid, search_id)
-  end
-
-  def stop(pid) do
-    Supervisor.stop_client(pid)
-  end
+  def start_supervisor, do: Supervisor.start_link(nil)
+  def new(ip, username, secret_key), do: Supervisor.start_client(ip, username, secret_key)
+  def submit_search(pid, query), do: Client.submit_search(pid, query)
+  def search_status(pid, search_id), do: Client.search_status(pid, search_id)
+  def search_result(pid, search_id), do: Client.search_result(pid, search_id)
+  def searches(pid, status), do: Client.searches(pid, status)
+  def allowed_data(pid, type), do: Client.allowed_data(pid, type)
+  def stop(pid), do: Supervisor.stop_client(pid)
 end
