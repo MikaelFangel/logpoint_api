25873fe2af4bcea1586173c1e8c6f31ab74a2c92
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-12-03 23:28:05 +0100

    chore: version bump to v1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LogpointApi.MixProject do
   def project do
     [
       app: :logpoint,
-      version: "0.2.3",
+      version: "1.0.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
