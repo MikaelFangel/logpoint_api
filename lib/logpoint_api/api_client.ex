8fa1d176ca31ca07511a32e934635d2ab32f1bf5
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2025-09-18 23:01:27 +0200

    generalize api client

--- lib/logpoint_api/api_client.ex
+++ lib/logpoint_api/api_client.ex
@@ -1,5 +1,6 @@
 defmodule LogpointApi.ApiClient do
-  @moduledoc false
+  @moduledoc """
+  """
 
   defmodule JWTAuth do
     @moduledoc false
@@ -13,41 +14,47 @@ defmodule LogpointApi.ApiClient do
     defstruct [:username, :secret_key]
   end
 
-  def get_request(url, %JWTAuth{token: token}, params \\ %{}) do
+  @default_opts [hackney: [:insecure], recv_timeout: :infinity]
+
+  def get(url, auth, params \\ %{})
+
+  def get(url, %JWTAuth{token: token}, params) do
     full_url = build_url_with_params(url, params)
     headers = [{"Authorization", "Bearer " <> token}]
 
-    case HTTPoison.get(full_url, headers, hackney: [:insecure], recv_timeout: :infinity) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, maybe_decode(body)}
-
-      {:ok, %HTTPoison.Response{status_code: status, body: body}} ->
-        {:error, {:http_error, status, maybe_decode(body)}}
-
-      {:error, reason} ->
-        {:error, reason}
-    end
+    full_url
+    |> HTTPoison.get(headers, @default_opts)
+    |> handle_response()
   end
 
-  def get_request(url, %LegacyAuth{username: username, secret_key: secret}, content_type) do
-    headers = [{"Content-Type", "application/json"}]
+  def get(url, %LegacyAuth{} = auth, params) do
+    query_params = build_legacy_params(auth, params)
+    full_url = build_url_with_params(url, query_params)
+
+    full_url
+    |> HTTPoison.get([], @default_opts)
+    |> handle_response()
   end
 
-  def post_requst(url, %LegacyAuth{username: username, secret_key: secret_key}, content_type) do
-    # The search api get allowed data uses x-www encoding for its post requests
-    headers =
-      case content_type do
-        :json -> [{"Content-Type", "application/json"}]
-        :urlencoded -> [{"Content-Type", "application/x-www-form-urlencoded"}]
-        _ -> []
-      end
+  def post(url, body, %JWTAuth{token: token}) do
+    json_body = encode_json(body)
+
+    headers = [
+      {"Content-Type", "application/json"},
+      {"Authorization", "Bearer " <> token}
+    ]
+
+    url
+    |> HTTPoison.post(json_body, headers, @default_opts)
+    |> handle_response()
   end
 
-  def post_request(url, body, %JWTAuth{token: token}) do
-    HTTPoison.post(url, body, [{"Content-Type", "application/json", "Authorization", "Bearer " <> token}],
-      hackney: [:insecure],
-      recv_timeout: :infinity
-    )
+  def post(url, body, %LegacyAuth{} = auth, content_type \\ :json) do
+    {encoded_body, headers} = prepare_legacy_post(auth, body, content_type)
+
+    url
+    |> HTTPoison.post(encoded_body, headers, @default_opts)
+    |> handle_response()
   end
 
   defp build_url_with_params(url, params) when params == %{} or params == [], do: url
@@ -55,6 +62,51 @@ defmodule LogpointApi.ApiClient do
   defp build_url_with_params(url, params) do
     query_string = URI.encode_query(params)
     "#{url}?#{query_string}"
+  end
+
+  defp build_legacy_params(%LegacyAuth{username: username, secret_key: secret}, additional_params) do
+    base_params = %{"username" => username, "secret_key" => secret}
+    Map.merge(base_params, additional_params)
+  end
+
+  defp prepare_legacy_post(%LegacyAuth{} = auth, body, content_type) do
+    case content_type do
+      :json ->
+        payload = build_legacy_json_payload(auth, body)
+        encoded = encode_json(payload)
+        headers = [{"Content-Type", "application/json"}]
+        {encoded, headers}
+
+      :urlencoded ->
+        payload = build_legacy_params(auth, body)
+        encoded = URI.encode_query(payload)
+        headers = [{"Content-Type", "application/x-www-form-urlencoded"}]
+        {encoded, headers}
+    end
+  end
+
+  defp build_legacy_json_payload(%LegacyAuth{username: username, secret_key: secret}, request_data) do
+    base = %{"username" => username, "secret_key" => secret}
+
+    case request_data do
+      nil -> base
+      data -> Map.put(base, "requestData", data)
+    end
+  end
+
+  defp encode_json(data) when is_binary(data), do: data
+  defp encode_json(data), do: Jason.encode!(data)
+
+  defp handle_response({:ok, %HTTPoison.Response{status_code: status, body: body}}) when status in 200..299 do
+    {:ok, maybe_decode(body)}
+  end
+
+  defp handle_response({:ok, %HTTPoison.Response{status_code: status, body: body}}) do
+    {:error, {:http_error, status, maybe_decode(body)}}
+  end
+
+  defp handle_response({:error, reason}) do
+    {:error, reason}
   end
 
   defp maybe_decode(msg) when is_binary(msg) do
