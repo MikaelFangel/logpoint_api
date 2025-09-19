f7a4176b1bd4c1fa13bacc0fddf622be532b1b3a
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2025-09-20 00:46:26 +0200

    initial http client for jwt

--- lib/logpoint_api/net/alert_rule_client.ex
+++ lib/logpoint_api/net/alert_rule_client.ex
@@ -1,3 +1,32 @@
 defmodule LogpointApi.Net.AlertRuleClient do
   @moduledoc false
+  def new(base_url, ssl_verify \\ true) do
+    base_options = [base_url: base_url]
+
+    options =
+      if ssl_verify do
+        base_options
+      else
+        base_options ++
+          [
+            connect_options: [
+              transport_opts: [
+                verify: :verify_none
+              ]
+            ]
+          ]
+      end
+
+    Req.new(options)
+  end
+
+  def get(req, path, token) do
+    req = Req.merge(req, auth: {:bearer, token})
+    Req.get(req, url: path)
+  end
+
+  def post(req, path, token, body) do
+    req = Req.merge(req, auth: {:bearer, token})
+    Req.post(req, url: path, json: body)
+  end
 end
